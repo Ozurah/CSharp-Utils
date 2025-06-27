7d300c12af634afb5bae53815a1ec535323a6543
Ozurah <80265286+Ozurah@users.noreply.github.com>
2025-06-27 19:13:38 +0200

    feat(Enum): IsDefined nullables + int

--- Utils/Utils/EnumUtils.cs
+++ Utils/Utils/EnumUtils.cs
@@ -1,4 +1,4 @@
-﻿namespace Ozurah.Utils.Enums
+namespace Ozurah.Utils.Enums
 {
     // Note : Nammed "Enum utils" (instead of simply "Enums") to avoid issue if call "Enums.XXX" inside our "Enum data classes" (their namespace override this one)
     // Note : Having generics is more "confortable" instead typing everytime "typeof(XXX)"
@@ -12,15 +12,24 @@
     {
         // IsDefined source : https://stackoverflow.com/questions/13248869/c-sharp-enum-contains-value
 
-        public static bool IsDefined(string name)
+        public static bool IsDefined(string? name)
         {
+            if (name is null) return false;
             return Enum.IsDefined(typeof(T), name);
         }
 
-        public static bool IsDefined(T value)
+        public static bool IsDefined(T? value)
         {
+            if (value is null) return false;
             return Enum.IsDefined(typeof(T), value);
         }
+
+        public static bool IsDefined(int? value)
+        {
+            if (value is null) return false;
+            return Enum.IsDefined(typeof(T), value);
+        }
+
 
         /// <summary>
         /// If <paramref name="value"/> is not defined for the enum, throw an <see cref="ArgumentException"/> with <paramref name="message"/>
